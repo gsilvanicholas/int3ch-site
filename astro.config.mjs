3fa0c90edfbefbed586c769c528b7929c66f2f63
Nicholas Silva <gsilva.nicholas@gmail.com>
2026-09-16 23:39:01 -0300

    Ajusta site/sitemap para URL de producao da Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: trocar pelo dominio definitivo assim que ele existir
-  site: 'https://int3ch.com.br',
+  // TODO: trocar pelo dominio definitivo quando comprar um
+  site: 'https://int3ch-site.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
